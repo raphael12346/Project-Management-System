3fff1d4b82944e8047b0a60e9313d72747ba6607
Raphael Orillano <raphael.orillano2@gmail.com>
2023-06-02 18:24:24 +0800

    Update DashBoardComponents.vue

--- Project Management System/src/components/DashBoardComponents.vue
+++ Project Management System/src/components/DashBoardComponents.vue
@@ -1,70 +1,98 @@
 <template>
     <div class="sidebar">
-        <div class="top-menu">
-            <img class = "logo" src = "../assets/logo.png" />
-        </div>
-        <div class="profile-panel">
-            <span id ="acount-icon" class="material-symbols-rounded">
-                account_circle
-            </span>
-            <div class="text">
-                <span class="text-name">
-                    Liza S. Orillano
-                </span>
-                <span class="text-occupation">
-                    Civil Engineer
-                </span>
+        <div class="upper">
+            <div class="top-menu">
+                <img class = "logo" src = "../assets/logo.png" />
             </div>
-            <div class="dropdown">
-                <span id="more-icon" class="material-symbols-rounded">
-                    more_horiz
+            <div class="profile-panel">
+                <span id ="acount-icon" class="material-symbols-rounded">
+                    account_circle
                 </span>
-                <div class="dropdown-content">
-                    <a href="#">Account Settings</a>
+                <div class="text">
+                    <span class="text-name">
+                        Liza S. Orillano
+                    </span>
+                    <span class="text-occupation">
+                        Civil Engineer
+                    </span>
+                </div>
+                <div class="dropdown">
+                    <span id="more-icon" class="material-symbols-rounded">
+                        more_horiz
+                    </span>
+                    <div class="dropdown-content">
+                        <a href="#">Account Settings</a>
+                    </div>
                 </div>
             </div>
+            <div class="panels">
+                <span id="lower-icon" class="material-symbols-rounded">
+                    dashboard
+                </span>
+                <span class = "text-panel">
+                    Dashboard
+                </span>
+            </div>
+            <div class="panels">
+                <span id="lower-icon" class="material-symbols-rounded">
+                    format_list_bulleted
+                </span>
+                <span class = "text-panel">
+                    All Surveys
+                </span>
+            </div>
+            <div class="panels">
+                <span id="lower-icon" class="material-symbols-rounded">
+                    pending_actions
+                </span>
+                <span class = "text-panel">
+                    Ongoing Surveys
+                </span>
+            </div>
         </div>
-        <div class="panels">
-            <span id="lower-icon" class="material-symbols-rounded">
-                dashboard
-            </span>
-            <span class = "text-panel">
-                Dashboard
-            </span>
-        </div>
-        <div class="panels">
-            <span id="lower-icon" class="material-symbols-rounded">
-                format_list_bulleted
-            </span>
-            <span class = "text-panel">
-                All Surveys
-            </span>
-        </div>
-        <div class="panels">
-            <span id="lower-icon" class="material-symbols-rounded">
-                pending_actions
-            </span>
-            <span class = "text-panel">
-                Ongoing Surveys
-            </span>
-        </div>
-        <div class="lower-panel">
+        <div class="lower">
             <div class="lower-panels">
-            <span id="lower-icon" class="material-symbols-rounded">
-                settings
-            </span>
-            <span class = "text-panel">
-                Settings
-            </span>
+                <span id="lower-icon" class="material-symbols-rounded">
+                    settings
+                </span>
+                <span class = "text-panel">
+                    Settings
+                </span>
+            </div>
+            <div class="lower-panels">
+                <span id="lower-icon" class="material-symbols-rounded">
+                    logout
+                </span>
+                <span class = "text-panel">
+                    Logout
+                </span>
+            </div>
         </div>
-        <div class="lower-panels">
-            <span id="lower-icon" class="material-symbols-rounded">
-                logout
-            </span>
-            <span class = "text-panel">
-                Logout
-            </span>
+    </div>
+    <div class="right-container">
+        <div class="upper-bar">
+            <div class="search-bar">
+
+            </div>
         </div>
+        <div class="count panel">
+            <div class="all-surveys-count">
+
+            </div>
+            <div class="ongoing-surveys-count">
+                
+            </div>
+        </div>
+        <div class="calender-panel">
+            <div class="calender">
+
+            </div>
+        </div>
+        <div class="all-suverys-panel">
+            
+        </div>
+        <div class="ongoing-surveys-panel">
+
         </div>
     </div>
 </template>
@@ -152,13 +180,13 @@ body{
 
 .lower-panels{
     width: 100%;
-    height: 65px;
+    height: 50px;
     display: flex;
     flex-direction: rows;
     justify-content: center;
 }
 
-.lower-panel{
+.lower{
     position: absolute; 
     bottom: 0; 
     width: 260px;
@@ -243,13 +271,6 @@ body{
 .dropdown:hover .dropdown-content{
     display: block;
 }
-.dropdown:hover .dropbtn{
-    background-color: #3e8e41;
-}
-
-
-
-
 
 </style>
 
